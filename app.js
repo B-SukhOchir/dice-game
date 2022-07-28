0571550bea098ed3480bdc6680fcb3832d2bc51b
Sukh-Ochir <sukhochir95.b@gmail.com>
2022-07-28 15:55:22 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // тоглогчийн ээлжийг хадгалах хувьсагч нэгдүгээр тоглогч 0, хоёрдугаар тоглогч 1
-var activePlayer = 1;
+var activePlayer = 0;
 
 // тоглогчийн оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -40,9 +40,44 @@ window.document.querySelector(".dice").style.display = "none";
 3.4. уг обьектийн зургийг өөрчилнө */
 /*addEventListener ( event iin ner, event bolohod yamr үйлдэл хийх функц байна) */
 var diceDom = window.document.querySelector(".dice");
+// шоог шидэх event listiner
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 хүртэл санамсаргүй нэг тоог гаргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  // шооны зургийг гаргаж ирнэ
   diceDom.style.display = "block";
+
+  // буусан санамсаргүй тоонл харгалзах  шооны зургийг вэб дээр гаргаж ирнэ
   diceDom.src = "dice-" + diceNumber + ".png";
-  alert("шоо буулаа : " + diceNumber);
+
+  //Буусан тоо нь нэгээс ялгаатай бол идэвхитэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ
+  if (diceNumber !== 1) {
+    // 1 ээс ялгаатай  тоо буулаа Буусан тоог тоглогчийн оноо дээр нэмж өгнө
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө
+    // хэрэв 1 буусан тохиолдолд энэ тоглогчийн оноог 0 болгонло
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    // тоглогчийн ээлжийг нөгөө тоглогч руу шилжүүлнэ
+    //Хэрэв идэвхитэй тоглогч нь 0 байвал идэвхитэй тоглогчийг 1 болго
+    //Хэрэв идэвхитэй тоглогч нь 1 байвал идэвхитэй тоглогчийг 0 болго
+    /*if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    } одоо энэ if нөхцөлийг арай товчхоноор бичье гурвалсан нөхцөл ашиглан */
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    //улаан цэгийг шилжүүлэх код add remove ийн оронд toggle өгчихвөл улаан цэгийг хооронд нь шилжүүлэх үйлдлийг хийнэ
+    /*document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.add("active");*/
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // шоог түр алга болгох
+  }
 });
